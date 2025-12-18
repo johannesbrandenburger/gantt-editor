50432de47e1070d0248e39f080018a60b0b320a9
Johannes Brandenburger <johannes.brandenburger@vanderlande-logistics.com>
2025-12-18 15:12:24 +0100

    export input types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import GanttEditorComponent from './components/GanttEditorComponent.vue'
+export type {
+    GanttEditorSlot,
+    GanttEditorDestination,
+    GanttEditorDestinationGroup,
+    GanttEditorMarkedRegion,
+    GanttEditorSuggestion,
+    GanttEditorSlotWithUiAttributes,
+    GanttEditorXAxisOptions,
+} from './components/gantt-editor-lib/chart/types.ts'
 
 // Named export
 export { GanttEditorComponent }
