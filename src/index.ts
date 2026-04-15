2a622a8912c98a0d2eb11cb646ce4e62d766bc97
Johannes Brandenburger <johannes.brandenburger@vanderlande-logistics.com>
2026-04-16 00:11:09 +0200

    no default vue exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-export * from './vue'
-export { default } from './vue'
+export type {
+	GanttEditorSlot,
+	GanttEditorSlotDeadline,
+	GanttEditorDestination,
+	GanttEditorDestinationGroup,
+	GanttEditorMarkedRegion,
+	GanttEditorSuggestion,
+	GanttEditorCanvasContextMenuAction,
+	GanttEditorSlotContextMenuAction,
+	GanttEditorVerticalMarker,
+	GanttEditorSlotWithUiAttributes,
+	GanttEditorXAxisOptions,
+} from './components/gantt-editor-lib/chart/types'
+
+export type {
+	GanttEditorProps,
+	GanttEditorCallbacks,
+	GanttEditorHost,
+	GanttEditorFeature,
+	GanttEditorRulerMode,
+} from './components/gantt-editor-lib/chart/gantt_canvas_props'
+
+export type { HelpOverlayTileDefinition } from './components/gantt-editor-lib/chart/help_overlay/help_overlay_tile'
+
+export {
+	getSelectionItemDisplayName,
+	getClipboardItemDisplayName,
+} from './components/gantt-editor-lib/chart/gantt_canvas_props'
